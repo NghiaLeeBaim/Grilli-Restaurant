ca8b9169f1b212ebb789f1702061bee4d675990f
Nghia <nghia0843309947@gmail.com>
2023-06-15 16:05:46 +0700

    2 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,25 @@ window.addEventListener("load", function () {
   preloader.classList.add("loaded");
   document.body.classList.add("loaded");
 });
+
+// add event listeners
+
+const addEventOnElenments = function (elements, evenType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(evenType, callback);
+  }
+};
+
+// navbar
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElenments(navTogglers, "click", toggleNavbar);
